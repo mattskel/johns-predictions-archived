955152da66a7769c30caef7669b82e71712aa194
Matt Skelley <mattskelley.au@gmail.com>
2022-12-05 20:44:49 +0800

    Update predictions form
    Use lifting state up to store the answers.

--- wine-one-frontend/src/components/PredictionsForm.js
+++ wine-one-frontend/src/components/PredictionsForm.js
@@ -4,17 +4,25 @@ import { QuestionsContextProvider } from '../context/QuestionsContext';
 // import PredictionInput from './PredictionInput'
 
 
-const PredictionInput = (({question}) => {
-  const [prediction, setPrediction] = useState('');
+const PredictionInput = (({question, onChange, value}) => {
+  // const [prediction, setPrediction] = useState('');
   return (
     <div className="prediction-container">
       <label>{question.text}</label>
-      <input 
+      {/* <select value={prediction} onChange={(e) => setPrediction(e.target.value)}> */}
+      <select value={value || ""} onChange={onChange} id={question._id}>
+      <option value="" disabled="disabled">Select an option</option>
+      {question.options && question.options.map((option) => (
+        <option value={option} key={option}>{option}</option>
+      ))}
+      </select>
+      
+      {/* <input 
         type="text" 
         className="prediction"
         value={prediction}
         onChange={(e) => setPrediction(e.target.value)}
-      />
+      /> */}
     </div>
   )
 });
@@ -22,10 +30,22 @@ const PredictionInput = (({question}) => {
 export default function PredictionsForm() {
   const {questions, dispatch} = useQuestionsContext();
   const prospectiveId = '638b2bb468447d08f7496271'
+  const [predictions, setPredictions] = useState({});
   // const questions = [{_id: '1', text: 'What are you going to do today?'}];
-  const handleSubmit = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    console.dir(predictions);
   };
+
+  const onPredictionChange = (event) => {
+    const prediction = event.target.value;
+    const questionId = event.target.id
+    setPredictions({
+      ...predictions,
+      [questionId]: prediction
+    })
+  }
 
   useEffect(() => {
     const fetchQuestions = async () => {
@@ -45,9 +65,16 @@ export default function PredictionsForm() {
     <form onSubmit={handleSubmit} className="predictions">
       <QuestionsContextProvider>
         {questions && questions.map((question) => (
-          <PredictionInput key={question._id} question={question} />
+          <PredictionInput 
+            key={question._id}
+            question={question}
+            onChange={onPredictionChange}
+            value={predictions[question._id]}
+          />
         ))}
         </QuestionsContextProvider>
+
+        <button>Submit form</button>
     </form>
   )
 }
