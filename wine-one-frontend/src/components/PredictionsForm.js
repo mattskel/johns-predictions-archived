13c761e19775ff2539913a81e48a99386baab81a
Matt Skelley <mattskelley.au@gmail.com>
2022-12-24 22:29:02 +1100

    Refactor predictions form

--- wine-one-frontend/src/components/PredictionsForm.js
+++ wine-one-frontend/src/components/PredictionsForm.js
@@ -1,43 +1,37 @@
 import {useState, useEffect} from 'react';
-import { useQuestionsContext } from '../hooks/useQuestionsContext';
-import { QuestionsContextProvider } from '../context/QuestionsContext';
+import { Link } from 'react-router-dom';
 import { useAuthContext } from '../hooks/useAuthContext';
-// import PredictionInput from './PredictionInput'
 
-
-const PredictionInput = (({question, onChange, value}) => {
-  // const [prediction, setPrediction] = useState('');
+const PredictionInput = (({question, onChange, value, className}) => {
   return (
     <div className="prediction-container">
       <label>{question.text}</label>
-      {/* <select value={prediction} onChange={(e) => setPrediction(e.target.value)}> */}
-      <select value={value || ""} onChange={onChange} id={question._id}>
+      <select value={value || ""} onChange={onChange} id={question._id} className={className}>
       <option value="" disabled="disabled">Select an option</option>
       {question.options && question.options.map((option) => (
         <option value={option} key={option}>{option}</option>
       ))}
       </select>
-      
-      {/* <input 
-        type="text" 
-        className="prediction"
-        value={prediction}
-        onChange={(e) => setPrediction(e.target.value)}
-      /> */}
     </div>
   )
 });
 
 export default function PredictionsForm() {
-  const {questions, dispatch} = useQuestionsContext();
+  // There is no need to use context for the questions
+  // The questions don't change so the page doesn't need to reload
+  const [questions, setQuestions] = useState([])
   const prospectiveId = '638b2bb468447d08f7496271'
+  // Can prepopulate the predictions
+  // Look up the user and prospective and return all predictions (if they exist)
   const [predictions, setPredictions] = useState({});
+  const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
   const {user} = useAuthContext();
-  // const questions = [{_id: '1', text: 'What are you going to do today?'}];
+  const [submitted, setSubmitted] = useState(false);
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.dir(predictions);
     const response = await fetch(`/api/prospectives/${prospectiveId}/submit`, {
       method: 'POST',
       headers: {
@@ -48,8 +42,17 @@ export default function PredictionsForm() {
     });
 
     const json = await response.json();
-    console.log(response);
-    console.log(json);
+    if (!response.ok) {
+      setError(json.error);
+      setEmptyFields(json.emptyFields);
+      return;
+    }
+
+    setError(null);
+    setEmptyFields([]);
+    setSubmitted(true);
+
+    // Go to the confirmation page
   };
 
   const onPredictionChange = (event) => {
@@ -62,39 +65,61 @@ export default function PredictionsForm() {
   }
 
   useEffect(() => {
-    const fetchQuestions = async () => {
-      const response = await fetch(`/api/questions?prospectiveId=${prospectiveId}`, {
-        headers: {
-          'Authorization': `Bearer ${user.token}`
-        }
-      })
-      const json = await response.json()
+    const fetchData = async () => {
+      const headers = {
+        'Authorization': `Bearer ${user.token}`
+      }
+      const questionsResponse = await fetch(`/api/questions?prospectiveId=${prospectiveId}`, {headers})
+      const questionsJson = await questionsResponse.json()
+      if (!questionsResponse.ok) {
+        return;
+      }
 
-      if (response.ok) {
-        dispatch({type: 'SET_QUESTIONS', payload: json});
+      setQuestions(questionsJson);
+
+      const predictionsResponse = await fetch(`/api/predictions?prospectiveId=${prospectiveId}`, {headers});
+      const predictionsJson = await predictionsResponse.json();
+      if (predictionsResponse.ok) {
+        const initialPredictions = predictionsJson.reduce((accumulator, currentValue) => {
+          const {questionId, prediction} = currentValue;
+          return {
+            ...accumulator,
+            [questionId]: prediction
+          }
+        }, {});
+        setPredictions(initialPredictions);
       }
     }
 
     if (user) {
-      fetchQuestions();
+      fetchData();
     }
-  }, [dispatch, user]);
+  }, [user]);
+
+  if (submitted) {
+    return (
+      <div className="confirmation">
+        <span>Form submitted. </span>
+        <Link to="/">Return home.</Link>
+      </div>
+    )
+  }
 
   return (
     
     <form onSubmit={handleSubmit} className="predictions">
-      <QuestionsContextProvider>
         {questions && questions.map((question) => (
           <PredictionInput 
             key={question._id}
             question={question}
             onChange={onPredictionChange}
             value={predictions[question._id]}
+            className={emptyFields.includes(question._id) ? 'error': ''}
           />
         ))}
-        </QuestionsContextProvider>
 
         <button>Submit form</button>
+        {error && <div className="error">{error}</div>}
     </form>
   )
 }
