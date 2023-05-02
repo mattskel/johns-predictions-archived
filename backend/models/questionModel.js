1e482eae64e5fc3511cfa6e14ada32957a56d9fe
Matt Skelley <mattskelley.au@gmail.com>
2023-05-02 22:38:47 +0800

    Add answer to question model
    Reinstate options array to question model.
    Refactor getQuestions endpoint.

--- backend/models/questionModel.js
+++ backend/models/questionModel.js
@@ -8,6 +8,12 @@ const questionSchema = new mongoose.Schema({
   prospectiveId: {
     type: String
   },
+  answer: {
+    type: String
+  },
+  options: {
+    type: [String]
+  }
 }, {timestamps: true})
 
 const Question = mongoose.model('Question', questionSchema);
