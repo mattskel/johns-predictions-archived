d4d35a4b395ff45b278e4ba80d56b3e8f19c503f
Matt Skelley <mattskelley.au@gmail.com>
2023-05-03 17:47:25 +0800

    Add questions and predictions page

---
+++ wine-one-frontend/src/pages/QuestionsAndPredictions.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function QuestionsAndPredictions() {
+  const [questionsAndPredictions, setQuestionsAndPredictions] = useState([]);
+  const { prospectiveId } = useParams();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user')) || {};
+    fetch(`/api/prospectives/${prospectiveId}/questions-and-predictions`, { headers: { Authorization: `Bearer ${user.token}` } })
+      .then((response) => response.json())
+      .then((response) => setQuestionsAndPredictions(response));
+  }, []);
+  return (
+    <div>
+      <h1>Questions & Predictions (& Answers)</h1>
+      <table className="fill-available">
+        <tbody className="alternating-background-color">
+          {questionsAndPredictions.map((questionAndPrediction) => {
+            let result;
+            if (questionAndPrediction.answer !== undefined) {
+              result = (questionAndPrediction.answer === questionAndPrediction.prediction)
+                ? '✅'
+                : '❌';
+            }
+            return (
+              <tr key={questionAndPrediction.questionId}>
+                <td>{questionAndPrediction.text}</td>
+                <td>{questionAndPrediction.prediction}</td>
+                <td colSpan="{questionAndPrediction.answer === undefined ? 2 : 1}">
+                  {questionAndPrediction.answer || <i>No answer</i>}
+                </td>
+                {questionAndPrediction.answer && <td>{result}</td>}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default QuestionsAndPredictions;
