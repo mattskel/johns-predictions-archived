7b88f6211484df5cf9390efa955b9e8af4a41192
Matt Skelley <mattskelley.au@gmail.com>
2023-06-02 21:26:26 +0800

    Update the home page
    Removing the submission link. Can come back to this.

--- wine-one-frontend/src/pages/Home.js
+++ wine-one-frontend/src/pages/Home.js
@@ -2,15 +2,16 @@
 import { Link } from 'react-router-dom';
 
 function Home() {
-  const prospectiveId = '638b2bb468447d08f7496271';
+  // const prospectiveId = '638b2bb468447d08f7496271';
 
   return (
     <div className="home">
-      Welcome! Preamble...
+      Welcome! Checkout how your predictions are tracking below.
       <div className="predictions">
-        <Link to={`../prospectives/${prospectiveId}/form`}>Submit your predictions</Link>
-        <br />
-        <Link to="../prospectives">Prospectives</Link>
+        {/* Can enable this next year */}
+        {/* <Link to={`../prospectives/${prospectiveId}/form`}>Submit your predictions</Link>
+        <br /> */}
+        <Link to="../prospectives">Your 2023 predictions</Link>
       </div>
     </div>
   );
