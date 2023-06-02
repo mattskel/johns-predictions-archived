2edcbe910dd658b7e2eaf47ccd05c4d4e6159db6
Matt Skelley <mattskelley.au@gmail.com>
2023-06-02 21:22:41 +0800

    Remove list from prospectives page
    Now the list is injected using react-router Outlet.

--- wine-one-frontend/src/pages/Prospectives.js
+++ wine-one-frontend/src/pages/Prospectives.js
@@ -1,37 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Breadcrumbs from '../components/Breadcrumbs';
+import { Outlet } from 'react-router-dom';
+import ProspectivesList from '../components/ProspectivesList';
 
-class Prospectives extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { prospectives: [] };
-  }
-
-  componentDidMount() {
-    const user = JSON.parse(localStorage.getItem('user')) || {};
-    fetch('/api/prospectives/', { headers: { Authorization: `Bearer ${user.token}` } })
-      .then((response) => response.json())
-      .then((prospectives) => this.setState({ prospectives }));
-  }
-
-  render() {
-    const { prospectives } = this.state;
-    return (
-      <div>
-        {/* <h1>Prospectives</h1> */}
-        <Breadcrumbs currentPage="Prospectives"/>
-        <ul>
-          {prospectives.map((prospective) => (
-            <li key={prospective._id}>
-              <Link to={`${prospective._id}/`} relative="path">{prospective.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function Prospectives() {
+  return (
+    <div className="prospectives">
+      <Breadcrumbs />
+      <Outlet />
+      {/* <ProspectivesList /> */}
+    </div>
+  );
 }
 
 export default Prospectives;
